b7b4e54cde05a4b224305ad290ebecb49ef083ff
CodeXRook <lacekitfly@gmail.com>
2019-02-05 21:28:20 -0500

    added eventHandler

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+constructor () {
+  super()
+
+  this.state={
+    displayValue: '0',
+  }
+}
+
+newNumDisplay =(e) =>{
+  let newNumber = e.target.value.toString();
+  let display = this.state.displayValue;
+  //numberToDisplay += ;
+  this.setState({ displayValue: display += newNumber});
+  console.log(this.state.displayValue)
+}
+
+
   render() {
     return (
 <>
         <div id="root">
     <div className="holder">
       <div className="calculator">
-      <div className="col-12 inputview">{}0</div>
+      <div  className="col-12 inputview">{this.state.displayValue}</div>
         <div className="row">
           <button className="button col-3">AC</button>
           <button className="button col-3">%</button>
@@ -16,25 +34,25 @@ class App extends Component {
           <button className="button col-3 orange">÷</button>
         </div>
         <div className="row">
-          <button className="button col-3">7</button>
-          <button className="button col-3">8</button> 
-          <button className="button col-3">9</button> 
-          <button className="button col-3 orange">x</button>
+          <button onClick={this.newNumDisplay} className="button col-3"value="7" >7</button>
+          <button onClick={this.newNumDisplay} className="button col-3"value="8" >8</button> 
+          <button onClick={this.newNumDisplay} className="button col-3"value="9" >9</button> 
+          <button onClick={this.newNumDisplay} className="button col-3 orange">x</button>
         </div>
         <div className="row">
-          <button className="button col-3">4</button> 
-          <button className="button col-3">5</button> 
-          <button className="button col-3">6</button>
-          <button className="button col-3 orange">-</button>
+          <button onClick={this.newNumDisplay} className="button col-3"value="4" >4</button> 
+          <button onClick={this.newNumDisplay} className="button col-3"value="5" >5</button> 
+          <button onClick={this.newNumDisplay} className="button col-3"value="6" >6</button>
+          <button  className="button col-3 orange">-</button>
         </div>
         <div className="row">
-          <button className="button col-3">1</button> 
-          <button className="button col-3">2</button>
-        <button className="button col-3">3</button>
+          <button onClick={this.newNumDisplay} className="button col-3"value="1" >1</button> 
+          <button onClick={this.newNumDisplay} className="button col-3" value="2" >2</button>
+        <button onClick={this.newNumDisplay} className="button col-3" value="3" >3</button>
          <button className="button col-3 orange">+</button>
          </div>
          <div className="row">
-        <button className="button col-6">0</button>
+        <button onClick={this.newNumDisplay} className="button col-6" value="0" >0</button>
         <button className="button col-3">.</button> 
         <button className="button col-3 orange">=</button>
         </div>
