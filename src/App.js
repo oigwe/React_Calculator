1b8380b716c01a6108aa6f8aff3914af230dab10
ositaigwe@pursuit.org <j0h@example.com>
2019-02-12 18:44:27 -0500

    changes props of Button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Buttons from './components/Buttons';
-import Display from './components/Display'
+import Display from './components/Display';
 
 class App extends Component {
 
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,38 +4,38 @@ import './Button.css'
 const Buttons = (props) => {
     return <>
          <div className="row">
-            <button onClick={props.handleClick} className="button col-3" data-action="AC">AC</button>
-            <button onClick={props.handleClick} className="button col-3" data-action="percent">%</button>
-            <button onClick={props.handleClick} className="button col-3" data-action="negPos">+/-</button>
-            <button onClick={props.handleClick} className="button col-3 orange" data-action="divide">/</button>
+            <button onClick={props.onClick} className="button col-3" data-action="AC">AC</button>
+            <button onClick={props.onClick} className="button col-3" data-action="percent">%</button>
+            <button onClick={props.onClick} className="button col-3" data-action="negPos">+/-</button>
+            <button onClick={props.onClick} className="button col-3 orange" data-action="divide">/</button>
           </div>
 
           <div className="row">
-            <button onClick={props.handleClick} className="button col-3" value="7">7</button>
-            <button onClick={props.handleClick} className="button col-3" value="8">8</button>
-            <button onClick={props.handleClick} className="button col-3" value="9">9</button>
-            <button onClick={props.handleClick} className="button col-3 orange" data-action="multiply">x</button>
+            <button onClick={props.onClick} className="button col-3" value="7">7</button>
+            <button onClick={props.onClick} className="button col-3" value="8">8</button>
+            <button onClick={props.onClick} className="button col-3" value="9">9</button>
+            <button onClick={props.onClick} className="button col-3 orange" data-action="multiply">x</button>
           </div>
 
           <div className="row">
-            <button onClick={props.handleClick} className="button col-3" value="4" >4</button>
-            <button onClick={props.handleClick} className="button col-3" value="5" >5</button>
-            <button onClick={props.handleClick} className="button col-3" value="6" >6</button>
-            <button className="button col-3 orange" data-action="subtract" onClick={props.handleClick}>-</button>
+            <button onClick={props.onClick} className="button col-3" value="4" >4</button>
+            <button onClick={props.onClick} className="button col-3" value="5" >5</button>
+            <button onClick={props.onClick} className="button col-3" value="6" >6</button>
+            <button className="button col-3 orange" data-action="subtract" onClick={props.onClick}>-</button>
           </div>
 
           <div className="row">
-            <button onClick={props.handleClick} className="button col-3" value="1" >1</button>
-            <button onClick={props.handleClick} className="button col-3" value="2" >2</button>
-            <button onClick={props.handleClick} className="button col-3" value="3" >3</button>
-            <button onClick={props.handleClick} className="button col-3 orange" data-action="add">+</button>
+            <button onClick={props.onClick} className="button col-3" value="1" >1</button>
+            <button onClick={props.onClick} className="button col-3" value="2" >2</button>
+            <button onClick={props.onClick} className="button col-3" value="3" >3</button>
+            <button onClick={props.onClick} className="button col-3 orange" data-action="add">+</button>
           </div>
 
         
           <div className="row">
-            <button onClick={props.handleClick} className="button col-6" value="0" >0</button>
-            <button className="button col-3" data-action="decimal" onClick={props.handleClick}>.</button>
-            <button className="button col-3 orange" data-action="equal" onClick={props.handleClick}>=</button>
+            <button onClick={props.onClick} className="button col-6" value="0" >0</button>
+            <button className="button col-3" data-action="decimal" onClick={props.onClick}>.</button>
+            <button className="button col-3 orange" data-action="equal" onClick={props.onClick}>=</button>
 
           </div>
     </>
